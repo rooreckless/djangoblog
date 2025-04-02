e21bc0d21c9c36d220a25bc003f358d959ac78f7
rooreckless <rooreckless@gmail.com>
2025-04-02 20:46:00 +0900

    @pytest.fixtureとconftest.pyの使い方についてコメントを記載

--- backend/api/tests/conftest.py
+++ backend/api/tests/conftest.py
@@ -2,7 +2,20 @@
 
 import pytest
 from rest_framework.test import APIClient
-
+# このファイルに書かれた@pytest.fixtureの内容は、このファイルが以下のディレクトリにあるtest_~.pyならば、引数にいきなりいれてok(import不要)
+# 例えば以下の状態だと、testsディレクトリ以下のtest_~.pyでapi_clientを引数に入れて、api_client()で実行することができる。
+# 実行した意味はrest_framework.test.APIClient()と同じになる。
 @pytest.fixture
 def api_client():
     return APIClient()
+
+# @pytest.fixtureにすべき内容としては、
+
+# 1. logined_userメソッドとか : ログインをしたうえで実行するテストがあるので、ユーザーを作成 -> ログイン状態にして返すメソッド
+# そのメソッドがあれば、テストケース内ではいきなりlogined_user()で「ログイン済みユーザー」での操作ができるようになる(テストケースの引数にもいれようね)
+
+# 2. 複数のモデルのデータやそのリレーションを一度に用意し、さらにそれを複数用意する前処理メソッド
+# 例えば、ブログとそれに紐づくタグとそのリレーション、さらにユーザーとそのリレーションをあらかじめ作成する場合
+
+# 3. あるモデルのレコードを複数用意する必要がある場合 : 中で、モデル.objects.createを何度も実行したり、モデル.createa_batch()をしたりする
+# fixtureの中で、固定のレコードを作成するでもいいし、factory_boyを使ってランダムにレコードを作成してもいい。
--- backend/api/tests/test_smoke.py
+++ backend/api/tests/test_smoke.py
@@ -44,4 +44,16 @@ def test_blog_retrieve_return_default_nouse_factory(db):
     assert data["contents_text"] == blog1.contents_text
 
 
+#---
+# 1でデコレータを使えるたり、2で引数にdbを使えるのはpytest-djangoによるものでdbがフィクスチャとして準備されているから
+# これを使用すると、テストケースのときにdbに値を保存したり、参照したりできるようになる。
+# またテストケースが終了したら、そのテストケースの中で登録したデータは消去され、次のテストに影響を与えない。
+#---
+# テストケースメソッドの引数に入れられたものは「`@pytest.fixture`デコレータで定義されたものかどうか」をまず判断する。
+# @pytest.fixtureがついているかどうかを探す箇所は、「同じテストケースファイル内 → そのテストケースファイルよりも親以上のディレクトリにあるconftest.pyファイル内 → 導入済みのpytest関連ライブラリ内」の順で探す。
+# もし見つかればその内容で使えるようになる。このとき、そのテストケースファイルではimportすることなく実行できる
+# dbは、pytest-djangoライブラリによりつかえるようになっているためである。(それを入れた意味については↑。またはhttps://blog.mtb-production.info/entry/2019/07/10/090000　で@pytest.mark.django_dbデコレータを使う意味と同じ。)
+# 引数にapi_clientを入れられる理由は、親ディレクトリにconftest.pyを作成し、その中で、api_clientメソッドに@pytest.fixtureデコレータがついているからである。
+# conftest.pyを参照してもらえば、@pytest.fixtureを使うべき状況が記述されています。
+#---
 # Factoryでテスト用のデータを作成させ、それをもとにテストをする場合などは、backend/api/tests/blogsのtest_~.pyを参照
