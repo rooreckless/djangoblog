98b3db37c979ac79c8f757f2c0a1353dcb338130
rooreckless <rooreckless@gmail.com>
2025-05-19 00:44:51 +0900

    さらにデバッグ用の記述を追加

--- aws_deploy/scripts/deploy.sh
+++ aws_deploy/scripts/deploy.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 set -e
-
+set -x  # ← 追加：実行されるコマンドをすべて出力
 echo "[deploy.sh] Starting application setup..."
 
 sudo dnf install -y tree
 
 echo "[deploy.sh] tree default"
-pwd
-ls -la
-tree . -a -L 3
+pwd > /tmp/pwd1.log
+ls -la > /tmp/ls1.log
+tree . -a -L 3 > /tmp/tree1.log 2>&1
 
 echo "[deploy.sh] tree top"
 cd /
-pwd
-ls -la
-tree . -a -L 3
+pwd > /tmp/pwd2.log
+ls -la > /tmp/ls2.log
+tree . -a -L 3 > /tmp/tree2.log 2>&1
 
 echo "[deploy.sh] tree cded"
 cd /home/ec2-user/app
-pwd
-ls -la
-tree . -a -L 3
+pwd > /tmp/pwd3.log
+ls -la > /tmp/ls3.log
+tree . -a -L 3 > /tmp/tree3.log 2>&1
 
 
 # Docker Compose 再起動（本番用のcomposeファイルがあればそちらに変更）
