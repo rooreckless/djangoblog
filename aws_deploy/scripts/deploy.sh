7d725ada2e71432188d191ec11daaa7ff2324f9d
rooreckless <rooreckless@gmail.com>
2025-05-24 19:09:42 +0900

    実験1

--- aws_deploy/scripts/deploy.sh
+++ aws_deploy/scripts/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # このスクリプト(codedeployのappspec.ymlでは、set -xやset -eは使えないことを確認)
+# スクリプト実行時の様子は、/opt/codedeploy-agent/deployment-root/<ランダムな数字>54c7fbda-2fb5-462d-98df-94ee1a0b8ed1/d-5GOOB6ZKC/logs
 echo "[deploy.sh] 実行開始"
 echo "[deploy.sh] カレントディレクトリの状況確認"
 pwd > /tmp/pwd1.log         # <- この出力は /
 ls -la > /tmp/ls1.log       # この中に、development.ymlが存在しない。あるとしたら、/home/ec2-user/appの中
- 
+echo $(pwd)
+echo $(ls -la)
+# cd /home/ec2-user/app
 
 
 # Docker Compose 再起動（本番用のcomposeファイルがあればそちらに変更）
