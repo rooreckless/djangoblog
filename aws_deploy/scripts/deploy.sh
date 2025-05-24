dd285df34c1bc832a7438687d1cd730d7bf9f145
rooreckless <rooreckless@gmail.com>
2025-05-24 12:29:13 +0900

    CodeBuildとCodeDeployのデバッグ用の表示を修正

--- aws_deploy/scripts/deploy.sh
+++ aws_deploy/scripts/deploy.sh
@@ -1,46 +1,28 @@
 #!/bin/bash
 set -e
 set -x  # ← 追加：実行されるコマンドをすべて出力
-echo "[deploy.sh] Starting application setup..."
-
-# sudo mkdir -p /home/ec2-user/app
-# sudo chown ec2-user:ec2-user /home/ec2-user/app
-
-sudo dnf install -y tree
-
-echo "[deploy.sh] tree default"
+echo "[deploy.sh] 実行開始"
+echo "[deploy.sh] カレントディレクトリの状況確認"
 pwd > /tmp/pwd1.log
 ls -la > /tmp/ls1.log
-tree . -a -L 3 > /tmp/tree1.log 2>&1
-
-echo "[deploy.sh] tree top"
-cd /
-pwd > /tmp/pwd2.log
-ls -la > /tmp/ls2.log
-tree . -a -L 3 > /tmp/tree2.log 2>&1
-
-echo "[deploy.sh] tree cded"
-cd /home/ec2-user/app
-pwd > /tmp/pwd3.log
-ls -la > /tmp/ls3.log
-tree . -a -L 3 > /tmp/tree3.log 2>&1
+# 
 
 
 # Docker Compose 再起動（本番用のcomposeファイルがあればそちらに変更）
-echo "[deploy.sh] Docker containers down"
+echo "[deploy.sh] コンテナを停止"
 docker compose -f development.yml down || true
-echo "[deploy.sh] Docker containers build & up -d"
+echo "[deploy.sh] コンテナビルドしなおしと、起動"
 docker compose -f development.yml build
 docker compose -f development.yml up -d
-echo "[deploy.sh] Docker containers are uped."
+echo "[deploy.sh] コンテナ起動完了"
 
 # 必要に応じてマイグレーションや静的ファイル収集
-echo "[deploy.sh] Docker backend_gunicorn start migrate."
+echo "[deploy.sh] バックエンド(gunicorn)コンテナ マイグレーション開始"
 docker compose -f development.yml run --rm backend_gunicorn python manage.py migrate
-echo "[deploy.sh] Docker backend_gunicorn start collectstatic."
+echo "[deploy.sh] バックエンド(gunicorn)コンテナ collectstatic 開始"
 docker compose -f development.yml run --rm backend_gunicorn python manage.py collectstatic --noinput
 
 # スーパーユーザー作成とサンプルデータ作成はEC2にログインして実施してください。
 
 
-echo "[deploy.sh] Deploy completed."
+echo "[deploy.sh] Deploy 完了."
