f1ebe41d9e6f9375e5d465ca65b0a6ff82fe5b15
rooreckless <rooreckless@gmail.com>
2025-05-25 01:11:31 +0900

    コメントを記載。

--- aws_deploy/scripts/deploy.sh
+++ aws_deploy/scripts/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # このスクリプト(codedeployのappspec.ymlでは、set -xやset -eは使えないことを確認)
-# スクリプト実行時の様子は、/opt/codedeploy-agent/deployment-root/<ランダムな数字>54c7fbda-2fb5-462d-98df-94ee1a0b8ed1/d-5GOOB6ZKC/logs
+# スクリプト実行時の様子は、/opt/codedeploy-agent/deployment-root/<ランダムな数字>/<デプロイid>/logs/scripts.logに書き込まれる
 echo "[deploy.sh] 実行開始"
 echo "[deploy.sh] カレントディレクトリの状況確認"
-pwd > /tmp/pwd1.log         # <- この出力は /
-ls -la > /tmp/ls1.log       # この中に、development.ymlが存在しない。あるとしたら、/home/ec2-user/appの中
-echo $(pwd)
-echo $(ls -la)
-cd /home/ec2-user/app
-echo $(pwd)
-echo $(ls -la)
+# pwd > /tmp/pwd1.log         # <- この出力は / であり、デバッグ用にファイルに書き込んでいる
+# ls -la > /tmp/ls1.log       # この中に、development.ymlが存在しない。あるとしたら、/home/ec2-user/appの中
+# echo $(pwd)                  # この出力は、/opt/codedeploy-agent/deployment-root/<ランダムな数字>/<デプロイid>/logs/scripts.logに書き込まれる <- やはり /
+# echo $(ls -la)
+cd /home/ec2-user/app          # appspec.ymlのdestinationに移動
+echo $(pwd)                    # <- ここで、/home/ec2-user/appに移動していることを確認         
+echo $(ls -la)                 # <- ここに　development.ymlをふくめリポジトリの内容があることを確認
 
 # Docker Compose 再起動（本番用のcomposeファイルがあればそちらに変更）
 echo "[deploy.sh] コンテナを停止"
